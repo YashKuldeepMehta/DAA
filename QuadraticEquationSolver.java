fc25c7fd7c3403000501d3ef0818fb685a3f0c77
Yash Mehta <113234432+YashKuldeepMehta@users.noreply.github.com>
2026-06-11 02:32:09 +0530

    Remove root calculation from findRoot method
    Removed the root calculation logic for quadratic equations.

--- QuadraticEquationSolver.java
+++ QuadraticEquationSolver.java
@@ -4,28 +4,7 @@ public class QuadraticEquationSolver {
     public void findRoot(double a, double b, double c) {
         double discriminant = b * b - 4 * a * c;
 
-        if (discriminant > 0) {
-            double root1 = (-b + Math.sqrt(discriminant)) / (2 * a);
-            double root2 = (-b - Math.sqrt(discriminant)) / (2 * a);
-
-            System.out.println("Roots are real and distinct:");
-            System.out.println("Root 1 = " + root1);
-            System.out.println("Root 2 = " + root2);
-
-        } else if (discriminant == 0) {
-            double root = -b / (2 * a);
-
-            System.out.println("Roots are real and equal:");
-            System.out.println("Root = " + root);
-
-        } else {
-            double realPart = -b / (2 * a);
-            double imaginaryPart = Math.sqrt(-discriminant) / (2 * a);
-
-            System.out.println("Roots are complex and imaginary:");
-            System.out.println("Root 1 = " + realPart + " + " + imaginaryPart + "i");
-            System.out.println("Root 2 = " + realPart + " - " + imaginaryPart + "i");
-        }
+   
     }
 
     public static void main(String[] args) {
